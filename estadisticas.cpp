b7df147e85484821f90308b96f6e027bb792f88e
unknown <saibot_iaconis99@hotmail.com>
2022-11-19 13:14:24 -0300

    Se corrigen algunas cosas en la funcion estadisticas

--- estadisticas.cpp
+++ estadisticas.cpp
@@ -13,15 +13,25 @@ void estadisticas(int *t, int *t2, char n[], char n2[], int *b, int *PuntajeMaxi
     cout << "----------------------Puntajes----------------------" << endl << endl;
     char NombreMaximo[20]= {};
 
+    //todo: cuando vuelvo a mostrar estadisticas desaparece el nombre
+    //del jugador con el puntaje maximo
     if(*t > *PuntajeMaximo)
     {
         *PuntajeMaximo = *t;
-        // NombreMaximo = n;
+
+        for(int x = 0; x < 20; x++)
+        {
+            NombreMaximo[x] = n[x];
+        }
     }
     //alo
     if(*t2 > *PuntajeMaximo)
     {
         *PuntajeMaximo = *t2;
+        for(int x = 0; x < 20; x++)
+        {
+            NombreMaximo[x] = n2[x];
+        }
     }
 
     if(*PuntajeMaximo == 0)
@@ -31,7 +41,8 @@ void estadisticas(int *t, int *t2, char n[], char n2[], int *b, int *PuntajeMaxi
 
     else
     {
-        cout << "El puntaje maximo es de: " << *PuntajeMaximo << endl << endl;
+        cout << "El puntaje maximo es de: " << *PuntajeMaximo <<
+             " por el jugador " << NombreMaximo << endl << endl;
 
     }
 
